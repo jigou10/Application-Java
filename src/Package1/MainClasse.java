c77b9fc8fbf91c389af4403a59ae430b9a6a0330
Jugurtha MAHDAD <jugurtha.mahdad@activops.fr>
2020-04-13 12:39:29 +0200

    first main

--- src/Package1/MainClasse.java
+++ src/Package1/MainClasse.java
@@ -4,7 +4,12 @@ public class MainClasse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Personne jugurtha=new Personne("Mahdad","jugurtha",23);
+		System.out.println("nom= "+jugurtha.getName()+" prenom= "+jugurtha.getSurname()+" age= "+jugurtha.getAge());
 		
 	}
 
 }
+
+
+//Hek3bk8JaJALvPG
